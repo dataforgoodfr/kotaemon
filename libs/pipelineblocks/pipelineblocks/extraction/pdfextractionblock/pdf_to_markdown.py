6f6e66af70dcc4d3c6015c7e925d91bce892dc0c
Jesshuan <jesshuan.dine@gmail.com>
2025-03-25 15:29:38 +0100

    Reapply "feat: customized taxonomy ecoskills rh document , fast_injestion_pipeline_rh OK, pipeline blocks involved for extraction inference"
    This reverts commit 0645e05bf9bec2d7e45c600a69595936b3b3cf39.
    
    ok

--- libs/pipelineblocks/pipelineblocks/extraction/pdfextractionblock/pdf_to_markdown.py
+++ libs/pipelineblocks/pipelineblocks/extraction/pdfextractionblock/pdf_to_markdown.py
@@ -1,5 +1,6 @@
 from pipelineblocks.extraction.pdfextractionblock.base import BasePdfExtractionBlock
 from pipelineblocks.extraction.pdf_utils.pymu import get_pymupdf4llm
+from typing import List
 
 class PdfExtractionToMarkdownBlock(BasePdfExtractionBlock):
 
@@ -10,7 +11,23 @@ class PdfExtractionToMarkdownBlock(BasePdfExtractionBlock):
             
         TODO implement other methods like 'split_by_chunks' with the length of each chunk, or 'split_by_parts' with a logic split by part... """
 
-    def run(self, pdf_path, method='split_by_page'):
+
+    def word_splitter(self, source_text: str) -> List[str]:
+        import re
+        source_text = re.sub("\s+", " ", source_text)  # Replace multiple whitespaces
+        return re.split("\s", source_text)  # Split by single whitespace
+    
+
+    def get_chunks_fixed_size_with_overlap(self, text: str, chunk_size: int, overlap: int) -> List[str]:
+        text_words = self.word_splitter(text)
+        chunks = []
+        for i in range(0, len(text_words), chunk_size):
+            chunk_words = text_words[max(i - overlap, 0): i + chunk_size]
+            chunk = " ".join(chunk_words)
+            chunks.append(chunk)
+        return chunks
+
+    def run(self, pdf_path, method='split_by_page', chunk_size: int =1024, chunk_overlap: int = 256):
 
         text_md_list = get_pymupdf4llm(pdf_path)
 
@@ -18,6 +35,14 @@ class PdfExtractionToMarkdownBlock(BasePdfExtractionBlock):
 
             return text_md_list
         
-        elif method == 'group_all':
+        else:
 
-            return "\n".join([c["text"] for c in text_md_list])
+            text_md_all = "\n".join([c["text"] for c in text_md_list])
+        
+            if method == 'group_all':
+                return text_md_all
+        
+            elif method == "split_by_chunk":
+                return self.get_chunks_fixed_size_with_overlap(text_md_all, chunk_size=chunk_size, overlap=chunk_overlap)
+
+
